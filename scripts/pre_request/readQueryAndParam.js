bbd00416b1c958b6a651016ef151bbe304e2dcd6
Ryan Lu <stones4586@gmail.com>
2024-10-04 09:16:42 +0800

    adding a script snippet

--- scripts/pre_request/readQueryAndParam.js
+++ scripts/pre_request/readQueryAndParam.js
@@ -1,48 +1,48 @@
 // 從 Collection Runner 中讀取 JSON 測試檔案的資料
 
-function paramAppend(req,json,key){
+function paramAppend(req,json){
     req.url?.variables.clear();
+    for(const key in json?.param??{}){
     req.url?.variables.add({
-        "key": "page_id",
-        "value":json?.param?.[key],
+        "key": key,
+        "value":json.param[key],
     })
+
+    }
+
  }
  
- function queryAppend(req,json,...key){
-     req.url?.query?.clear();
-    key.forEach((x)=>{
- 
-         if(json?.query?.[x]&&Array.isArray(json?.query?.[x])){
-                 for(const index in json.query[x]){
-                     req.url?.query?.add({
-                             key:`${x}[]`,
-                             value:json.query[x][index],
-                             disabled:false,
-                         })
-                 }
-       
- 
-         }else if(json?.query?.[x]){
- 
-                req.url?.query?.add({
-                 key:x,
-                 value:json.query[x],
-                 disabled:false,
-             })
-         }
-        
-       
- 
-    })
-     console.log(req.url?.query);
- }
+function queryAppend(req,json){
+    req.url?.query?.clear();
+    for(const key in json?.query??{}){
+       if(Array.isArray(json.query[key])){
+            for(const index in json.query[key]){
+                    req.url?.query?.add({
+                            key:`${x}[]`,
+                            value:json.query[key][index],
+                            disabled:false,
+                        })
+       }
+   }
+       else{
+
+       req.url?.query?.add({
+           key:x,
+           value:json.query[key],
+           disabled:false,
+       })//end add
+   } // end else
+  
+}// end for
+} // end queryAppend
+
  
  let jsonData = pm.iterationData.toObject();
  // 遍歷每個參數，將 `disabled` 設為 false
  if(pm.environment.get('cli')===true){
      pm.request.headers.add({key: 'Cookie', value:  pm.environment.get('cookies')})
-     queryAppend(pm.request,jsonData,'view_mode','page_ids','create_month_range','create_date_range');
-     paramAppend(pm.request,jsonData,'page_id');
+     queryAppend(pm.request,jsonData);
+     paramAppend(pm.request,jsonData);
    
  }
  
